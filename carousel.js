d6780e20938ce229d8c4020592bce26816be6a2b
Andrew Faircloth <andrewkfaircloth@gmail.com>
2015-10-06 22:05:13 -0400

    initial commit

---
+++ carousel.js
@@ -0,0 +1,34 @@
+var carouselImages = [
+  {
+    image_url: 'http://interfacelift.com/wallpaper/Debbfb5b/03966_upondescent_1280x720.jpg',
+    title: 'Upon Descent',
+    photographer_id: 'Phil2001'
+  },
+
+  $('#image') = this.show(image_url);
+  return this;
+
+  {
+    image_url: 'https://interfacelift.com/wallpaper/7yz4ma1/03965_sunsetinthecascades_1280x720.jpg',
+    title: 'Sunset in the Cascades',
+    photographer_id: 'smpoutdoors'
+  },
+  {
+    image_url: 'https://interfacelift.com/wallpaper/7yz4ma1/03958_seljalandsfossiceland_1280x720.jpg',
+    title: 'Seljalandsfoss, Iceland',
+    photographer_id: 'Robin Kamp'
+  },
+  {
+    image_url: 'https://interfacelift.com/wallpaper/7yz4ma1/03950_godsrays_1280x720.jpg',
+    title: 'Gods Rays',
+    photographer_id: 'illuminati'
+  }
+];
+
+
+console.log(carouselImages);
+console.log(carouselImages.title);
+
+$('#image').show(function() {
+  return carouselImages.attr(image_url);
+});
